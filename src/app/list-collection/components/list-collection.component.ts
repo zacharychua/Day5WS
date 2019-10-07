f5aa6bca13745822c9a49ffab61c00d725890564
zacky <E0284042@u.nus.edu>
2019-10-07 11:45:37 +0800

    Added list-album module

---
+++ src/app/list-collection/components/list-collection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-collection',
+  templateUrl: './list-collection.component.html',
+  styleUrls: ['./list-collection.component.css']
+})
+export class ListCollectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/list-collection/list-collection.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListCollectionComponent } from './components/list-collection.component';
+
+
+
+@NgModule({
+  declarations: [ListCollectionComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ListCollectionModule { }
